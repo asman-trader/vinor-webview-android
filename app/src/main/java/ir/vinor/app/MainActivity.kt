c47b2603daaa482056c6b5f37f07127b4604c9c8
Aseman <masood1528014@gmail.com>
2025-12-25 10:42:34 +0330

    Build vinor webview android apk

---
+++ app/src/main/java/ir/vinor/app/MainActivity.kt
@@ -0,0 +1,53 @@
+package ir.vinor.app
+
+import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import ir.vinor.app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val targetUrl = "https://vinor.ir"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        configureWebView()
+        binding.webView.loadUrl(targetUrl)
+    }
+
+    override fun onBackPressed() {
+        if (binding.webView.canGoBack()) {
+            binding.webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun configureWebView() {
+        with(binding.webView.settings) {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            useWideViewPort = true
+            loadWithOverviewMode = true
+            builtInZoomControls = true
+            displayZoomControls = false
+        }
+
+        binding.webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                request?.url?.toString()?.let { view?.loadUrl(it) }
+                return true
+            }
+        }
+    }
+}
+
